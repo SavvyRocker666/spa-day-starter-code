86b32a4361d59a5805d202cb0e0c077a559b477b
Savvy <sabrinarocker16@gmail.com>
2022-05-23 19:36:28 -0500

    added bonus mission from day 1

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,8 +1,8 @@
-package org.launchcode.spaday.controllers;
-
-public class UserController {
-displayAddUserForm()
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        // add form submission handling code here
-    }
-}
+//package org.launchcode.spaday.controllers;
+//
+//public class UserController {
+//displayAddUserForm()
+//    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+//        // add form submission handling code here
+//    }
+//}
